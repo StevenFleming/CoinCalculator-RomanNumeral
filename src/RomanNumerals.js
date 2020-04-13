c4f5899a24e2d74d518ae1386e21cb36b0442fed
stevenfleming <12flemings@gmail.com>
2020-04-13 16:23:20 -0700

    initial solution for converting roman numerals to ints

--- src/RomanNumerals.js
+++ src/RomanNumerals.js
@@ -10,15 +10,31 @@ const RomanNumeralValues = {
 
 const convertRomanToInt = (roman) => {
   const numberArray = roman.split("").map((e) => (e = RomanNumeralValues[e]));
-  return numberArray;
+  return numberComparison(numberArray);
   // return numberArray.reduce(function (currentVal, element) {
   //   return element + currentVal;
   // }, 0);
 };
 
+const numberComparison = (numberArray) => {
+  let sum = 0;
+  for (let i = 0; i < numberArray.length - 1; i++) {
+    if (numberArray[i] < numberArray[i + 1]) {
+      sum -= numberArray[i];
+    } else {
+      sum += numberArray[i];
+    }
+  }
+  return (sum += numberArray[numberArray.length - 1]);
+};
+
 console.log("IV: " + convertRomanToInt("iv"));
 console.log("III: " + convertRomanToInt("iii"));
 console.log("IX: " + convertRomanToInt("ix"));
+console.log("LXVI: " + convertRomanToInt("lxvi"));
+console.log("XCIX: " + convertRomanToInt("xcix"));
+
+console.log("Array Total + numberComparison");
 
 // const summedArray = numArray.reduce(function (currentValue, element) {
 //   return element + currentValue;
