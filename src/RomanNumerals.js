1918a0bf8fe4ad77222e7ff8de18c0f9188854a0
stevenfleming <12flemings@gmail.com>
2020-04-13 16:02:05 -0700

    add the correct numbers for each roman numeral and return

--- src/RomanNumerals.js
+++ src/RomanNumerals.js
@@ -1,14 +1,25 @@
 const RomanNumeralValues = {
-  i = 1,
-  v = 5,
-  x = 10,
-  l = 50,
-  c = 100,
-  d = 500,
-  m = 1000
-}
+  i: 1,
+  v: 5,
+  x: 10,
+  l: 50,
+  c: 100,
+  d: 500,
+  m: 1000,
+};
 
 const convertRomanToInt = (roman) => {
-  const numberArray = roman.oCharArray().map((c) => c = RomanNumeralValues(c));
+  const numberArray = roman.split("").map((e) => (e = RomanNumeralValues[e]));
   return numberArray;
-}
+  // return numberArray.reduce(function (currentVal, element) {
+  //   return element + currentVal;
+  // }, 0);
+};
+
+console.log("IV: " + convertRomanToInt("iv"));
+console.log("III: " + convertRomanToInt("iii"));
+console.log("IX: " + convertRomanToInt("ix"));
+
+// const summedArray = numArray.reduce(function (currentValue, element) {
+//   return element + currentValue;
+// }, 0);
